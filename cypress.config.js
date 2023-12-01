2456d2f8b1f936ebecf779887e943192ea41c11d
Roman Burlaka <burlakarv@gmail.com>
2023-12-01 17:28:55 +0100

    alias, fixtures

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    //baseUrl : 'https://demoqa.com/tool-tips',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 7000,
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  
   },
 });
---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,7 @@
+//Examples of trics
+
+//Close advertising:
+// cy.wait(8000)
+// .then(()=> {
+//  cy.wrap(window.top.document.querySelector('#closeLargeModal')).click({force: true})
+// })
