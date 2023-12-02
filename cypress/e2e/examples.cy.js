530b888d3ecee7b1a11e838ddc69c2e986026c3d
Roman Burlaka <burlakarv@gmail.com>
2023-12-02 22:19:27 +0100

    webTables

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,7 +1,14 @@
 //Examples of trics
 
 //Close advertising:
-// cy.wait(8000)
-// .then(()=> {
-//  cy.wrap(window.top.document.querySelector('#closeLargeModal')).click({force: true})
-// })
+        // cy.wait(8000)
+        // .then(()=> {
+        //  cy.wrap(window.top.document.querySelector('#closeLargeModal')).click({force: true})
+        // })
+//find element in array
+   // cy.get('.rt-tbody .rt-tr:not(.-pad-row) .rt-td').then($els=> {
+           //  const newArr = Cypress.$.makeArray($els).map($el => $el.innerText).slice(0,-64)
+            // console.log(newArr);
+            // console.log(testUser);
+            //expect(testUser).to.be.equal(newArr)
+        // })
