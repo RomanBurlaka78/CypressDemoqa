95fc3e0f8ce06c33311defb9aac1984634316d6d
Roman Burlaka <burlakarv@gmail.com>
2023-12-06 00:00:44 +0100

    POM refactor

---
+++ cypress/e2e/pom/ElementsPage.js
@@ -0,0 +1,17 @@
+import TextBoxPage  from './TextBoxPage';
+
+class ElementsPage {
+
+    getTextBox = ()=> cy.get('div.accordion>:first-child .menu-list> :first-child');
+
+
+
+    clickGetTexBox() {
+        this.getTextBox().click();
+
+        return new TextBoxPage();
+    }
+
+}
+
+export default ElementsPage;
