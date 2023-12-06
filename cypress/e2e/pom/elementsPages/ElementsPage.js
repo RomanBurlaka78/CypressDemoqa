4130bcfb4d6d7a9c4863da392698045955790b68
Roman Burlaka <burlakarv@gmail.com>
2023-12-06 19:29:05 +0100

    test

--- cypress/e2e/pom/elementsPages/ElementsPage.js
+++ cypress/e2e/pom/elementsPages/ElementsPage.js
@@ -1,12 +1,18 @@
-import TextBoxPage  from './TextBoxPage';
-
+import CheckBoxPage from './CheckBoxPage';
 class ElementsPage {
     getTextBox = ()=> cy.get('div.accordion>:first-child .menu-list> :first-child');
+    getCheckBox = () =>cy.get('div.accordion>:first-child .menu-list> :first-child+li');
 
     clickGetTexBox() {
         this.getTextBox().click();
 
         return new TextBoxPage();
+    }
+
+    clickGetCheckBox() {
+        this.getCheckBox().click();
+
+        return new CheckBoxPage();
     }
 }
 
---
+++ cypress/e2e/tests_pom/testPom_checkBox.cy copy.js
@@ -0,0 +1,28 @@
+/// <reference types = "cypress" />
+
+import HomePage from "../pom/HomePage";
+import variables from "../../fixtures/variables.json";
+
+describe('suit Elements', ()=> {
+    const homePage =new HomePage();
+
+    beforeEach(()=> {
+        homePage.navigateHomePage(variables.URL.site);  
+    })
+    it('test checkBox', ()=> {
+        homePage
+            .gotoElementsPage()
+            .clickGetCheckBox()
+            .clickGetcheckElement()
+            .getCheckElement().should('be.visible');       
+    })
+
+    it('test checkBox dropdown', ()=> {
+        homePage
+            .gotoElementsPage()
+            .clickGetCheckBox()
+            .clickGetArrowCheckBox()
+            .clickDropDownElement();      
+    })
+
+})
